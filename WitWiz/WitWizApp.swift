cbfe07bfd6ff19e7a6ef0c5688f63165675e94b1
Mounir Ybanez <rinuom91@gmail.com>
2025-05-28 13:19:05 +0800

    Changed window width to 1280.

--- WitWiz/WitWizApp.swift
+++ WitWiz/WitWizApp.swift
@@ -5,7 +5,7 @@ struct WitWizApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .frame(width: 1080, height: 720)
+                .frame(width: 1280, height: 720)
         }
         .windowResizability(.contentSize)
     }
